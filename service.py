69bdda1efb756ef81011a7e078f6bb328777a154
akaankshpemmaraju <54031567+akaankshpemmaraju@users.noreply.github.com>
2023-11-09 00:04:52 -0600

    Add files via upload

---
+++ service.py
@@ -0,0 +1,18 @@
+import csv
+
+from drugprediction.models import DrugReviewModel
+from drugprediction.sentimentanalyzer import getCommentSentiment
+
+def loadData():
+
+    #DrugReviewModel.objects.all().delete()
+
+    with open(file='C:\\Users\\tejitha reddy\\PycharmProjects\\DrugPredictionBasedOnSentimentAnalysisOfReviews\\dataset\\drugsComTrain_raw.csv',mode='r',encoding="utf8") as file:
+        reader = csv.reader(file)
+        for row in reader:
+           model=DrugReviewModel()
+           model.drugname=row[0]
+           model.condition=row[1]
+           model.review=row[2]
+           model.sentiment=getCommentSentiment(row[2])
+           model.save()
