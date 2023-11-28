5b5d5748349dde707f3535e593100e929933571c
akaankshpemmaraju <54031567+akaankshpemmaraju@users.noreply.github.com>
2023-11-27 18:31:26 -0600

    Update admin.py

--- admin.py
+++ admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from drugprediction.models import DrugReviewModel, PatientModel
 admin.site.register(PatientDetails)
 admin.site.register(DrugReviewModel)
+admin.site.register(DrugFinalModel)
