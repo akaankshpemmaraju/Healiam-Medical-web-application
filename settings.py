264ba4121adea7d112cfb32c75e2c7631c653538
thindiashokreddy <145074437+thindiashokreddy@users.noreply.github.com>
2023-11-29 18:54:22 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -31,6 +31,18 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'drugprediction.apps.DrugpredictionConfig',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
