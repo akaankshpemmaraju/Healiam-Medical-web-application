bd6342f392f028b0fac9597791dc6fe83ee4d366
Sai Sricharan Ayilavarapu <57758074+charan371@users.noreply.github.com>
2023-11-29 18:50:05 -0600

    Update forms.py

--- forms.py
+++ forms.py
@@ -4,12 +4,12 @@ class DrugReview(Form):
     drugname=CharField(max_length=500)
     condition=CharField(max_length=500)
     review=CharField(max_length=5000)
-
+#patient form
 class PatientForm(Form):
     name=CharField(max_length=50)
     mobile=CharField(max_length=50)
     drug=CharField(max_length=50)
-    
+#login form   
 class LoginForm(Form):
     username = CharField(max_length=100)
     password = CharField(widget=PasswordInput())
