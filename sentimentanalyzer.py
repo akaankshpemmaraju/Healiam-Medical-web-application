5571bbceae879c6430efca23478208fbb50dab96
nallabirudumahesh <147668415+nallabirudumahesh@users.noreply.github.com>
2023-11-29 19:43:57 -0600

    Update sentimentanalyzer.py

--- sentimentanalyzer.py
+++ sentimentanalyzer.py
@@ -1,14 +1,14 @@
 import re
 from textblob import TextBlob
 
-def cleanComment(cmnt):
+def cleanComment(cmnt)
     '''
-    Utility function used to clean tweet text by removing links, special characters
-    using simple regex statements.
+    Utility function used to clean tweet text by removing links special characters
+    using simple regex statements
     '''
-    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", cmnt).split())
+    return ' ' join(re sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"  " "  cmnt).split())
 
-def getCommentSentiment(cmnt):
+def getCommentSentiment(cmnt)
     '''
     Utility function will classify sentiment of passed tweets
     using textblob's sentiment method
@@ -17,9 +17,9 @@ def getCommentSentiment(cmnt):
     # creating TextBlob object of passed tweet text
     analysis = TextBlob(cleanComment(cmnt))
     # set sentiment on the required analytics
-    if analysis.sentiment.polarity > 0:
+    if analysis sentiment polarity > 0
         return 'positive'
-    elif analysis.sentiment.polarity == 0:
+    elif analysis sentiment polarity == 0
         return 'neutral'
-    else:
+    else
         return 'negation'
