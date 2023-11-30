3ff3fd6a6202791723d0650f2cbbc86adb443c17
thindiashokreddy <145074437+thindiashokreddy@users.noreply.github.com>
2023-11-29 18:55:05 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DrugPredictionBasedOnSentimentAnalysisOfReviews.settings')
 
 application = get_wsgi_application()
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DrugPredictionBasedOnSentimentAnalysisOfReviews.settings')
+
+application = get_wsgi_application()
