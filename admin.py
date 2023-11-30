08aca0cab4191febae27f21d1fd6c0bf87f28e0c
vinaykusuma57 <145311932+vinaykusuma57@users.noreply.github.com>
2023-11-29 18:44:23 -0600

    Update admin.py

--- admin.py
+++ admin.py
@@ -3,3 +3,4 @@ from drugprediction.models import DrugReviewModel, PatientModel
 admin.site.register(PatientDetails)
 admin.site.register(DrugReviewModel)
 admin.site.register(DrugFinalModel)
+#this is admin python file
