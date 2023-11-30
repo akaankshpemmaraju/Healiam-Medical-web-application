c84eb8e536c1fb6c1087727dfc3224a3ad15dce4
nallabirudumahesh <147668415+nallabirudumahesh@users.noreply.github.com>
2023-11-29 19:47:29 -0600

    Update service.py

--- service.py
+++ service.py
@@ -1,19 +1,19 @@
 import csv
 
-from drugprediction.models import DrugReviewModel
-from drugprediction.sentimentanalyzer import getCommentSentiment
+from drugprediction models import DrugReviewModel
+from drugprediction sentimentanalyzer import getCommentSentiment
 
-def loadData():
+def loadData()
     
-    #DrugReviewModel.objects.all().delete()
-# To be able to add the services.
-    with open(file=''C:\\Users\\tejitha reddy\\PycharmProjects\\DrugPredictionBasedOnSentimentAnalysisOfReviews\\dataset\\drugsComTrain_raw.csv',mode='r',encoding="utf8") as file:
-        reader = csv.reader(file)
-        for row in reader:
+    #DrugReviewModel objects all() delete()
+# To be able to add the services
+    with open(file=''C \\Users\\tejitha reddy\\PycharmProjects\\DrugPredictionBasedOnSentimentAnalysisOfReviews\\dataset\\drugsComTrain_raw csv' mode='r' encoding="utf8") as file
+        reader = csv reader(file)
+        for row in reader
            model=DrugReviewModel()
-           model.drugname=row[0]
-           model.condition=row[1]
-           model.review=row[2]
+           model drugname=row[0]
+           model condition=row[1]
+           model review=row[2]
 #getting the comments for the sentiment
-           model.sentiment=getCommentSentiment(row[2])
-           model.save()
+           model sentiment=getCommentSentiment(row[2])
+           model save()
