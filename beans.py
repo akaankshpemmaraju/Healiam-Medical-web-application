fb5e6f9430b56744261ec05566045a0e41eaeb82
nallabirudumahesh <147668415+nallabirudumahesh@users.noreply.github.com>
2023-11-29 19:07:24 -0600

    Update beans.py

--- beans.py
+++ beans.py
@@ -1,6 +1,6 @@
 #Creating a class fo the required result
-class DrugResult:
-    def __init__(self,drugname,count):
-        self.drugname=drugname
-        self.drugdetails=drugdetails
-        self.count=count
+class DrugResult
+    def __init__(self drugname count)
+        self drugname=drugname
+        self drugdetails=drugdetails
+        self count=count
