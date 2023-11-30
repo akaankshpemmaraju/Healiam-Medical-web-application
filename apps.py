a756a85ddd8646dce093542a7b7b01de445c2e46
thindiashokreddy <145074437+thindiashokreddy@users.noreply.github.com>
2023-11-29 18:53:18 -0600

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
 class DrugpredictionConfig(AppConfig):
     name = 'drugprediction'.
-    class DrugpredictionConfig(AppConfig):
-    name = 'drugprediction'.
-
+    
