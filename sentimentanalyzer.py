539cfc090ad72e50a361a23a64da8f13de3b71c9
Anil Reddy Gokha <145625892+anilreddygokha09@users.noreply.github.com>
2023-11-29 18:56:07 -0600

    Update sentimentanalyzer.py

--- sentimentanalyzer.py
+++ sentimentanalyzer.py
@@ -3,7 +3,7 @@ from textblob import TextBlob
 
 def cleanComment(cmnt):
     '''
-    Utility function to clean tweet text by removing links, special characters
+    Utility function used to clean tweet text by removing links, special characters
     using simple regex statements.
     '''
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", cmnt).split())
