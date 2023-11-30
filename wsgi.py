5a1dd27e03349d825d5d87582e15ee801ddb61f0
nallabirudumahesh <147668415+nallabirudumahesh@users.noreply.github.com>
2023-11-29 20:40:46 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for DrugPredictionBasedOnSentimentAnalysisOfReviews project.
+WSGI config for DrugPredictionBasedOnSentimentAnalysisOfReviews project
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``application``
 
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+For more information on this file see
+https://docs djangoproject com/en/3 0/howto/deployment/wsgi/
 """
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django core wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DrugPredictionBasedOnSentimentAnalysisOfReviews.settings')
+os environ setdefault('DJANGO_SETTINGS_MODULE'  'DrugPredictionBasedOnSentimentAnalysisOfReviews settings')
 
 application = get_wsgi_application()
