636055d6740978ca4d12513de8a0ececad9c336d
Sriniketh08 <89509015+Sriniketh08@users.noreply.github.com>
2023-11-29 18:47:26 -0600

    Update admin.py

--- admin.py
+++ admin.py
@@ -5,4 +5,4 @@ admin.site.register(PatientDetails)
 #reviews of drugs
 admin.site.register(DrugReviewModel)
 admin.site.register(DrugFinalModel)
-#this is admin python files
+#this is admin python files.
