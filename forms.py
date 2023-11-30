ca771030872ea5db67add9e11322ce2791cb78d1
nallabirudumahesh <147668415+nallabirudumahesh@users.noreply.github.com>
2023-11-29 19:08:32 -0600

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,16 +1,16 @@
-from django.forms import Form, CharField, PasswordInput
+from django forms import Form CharField PasswordInput
 # defining variables 
-class DrugReview(Form):
+class DrugReview(Form)
     drugname=CharField(max_length=500)
     condition=CharField(max_length=500)
     review=CharField(max_length=5000)
 #patient form
-class PatientForm(Form):
+class PatientForm(Form)
     name=CharField(max_length=50)
     mobile=CharField(max_length=50)
     drug=CharField(max_length=50)
 #login form   
-class LoginForm(Form):
+class LoginForm(Form)
     username = CharField(max_length=100)
     password = CharField(widget=PasswordInput())
 
