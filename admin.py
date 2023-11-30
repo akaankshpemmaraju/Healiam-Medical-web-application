07b9f7706d963c03ac7413445b6001fc187ee5cf
sathvik1507 <145074405+sathvik1507@users.noreply.github.com>
2023-11-29 18:54:21 -0600

    Update admin.py

--- admin.py
+++ admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from drugprediction.models import DrugReviewModel, PatientModel
 #patient details.
 admin.site.register(PatientDetails)
+
 #reviews of drugs.
 admin.site.register(DrugReviewModel)
 admin.site.register(DrugFinalModel)
